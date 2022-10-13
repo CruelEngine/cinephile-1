adf605591dff2e8d269a9a3c25ff48e276064840
Umang Bhatt <umang@incubyte.co>
2022-10-13 12:02:43 +0530

    CLN - Formatting

--- src/test/java/com/example/MovieServiceShould.java
+++ src/test/java/com/example/MovieServiceShould.java
@@ -1,12 +1,10 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,10 +14,10 @@ class MovieServiceShould {
   @DisplayName("movie service should call gateway")
   public void movie_service_should_call_gateway() {
     MovieGateway movieGateway = mock(MovieGateway.class);
-    MovieService  movieService = new MovieService(movieGateway);
-    Movie movie  = new Movie(1, "name", "image", 9.3f, 234, "2019-1-1");
-     List<Movie> movies = List.of(movie);
-    TmdbResponse tmdbResponse =   new TmdbResponse(1,movies  );
+    MovieService movieService = new MovieService(movieGateway);
+    Movie movie = new Movie(1, "name", "image", 9.3f, 234, "2019-1-1");
+    List<Movie> movies = List.of(movie);
+    TmdbResponse tmdbResponse = new TmdbResponse(1, movies);
     given(movieGateway.search("name")).willReturn(tmdbResponse);
     movieService.search("name");
     verify(movieGateway).search("name");
@@ -29,7 +27,7 @@ class MovieServiceShould {
   @DisplayName("get by id calls gateway")
   public void get_by_id_calls_gateway() {
     MovieGateway movieGateway = mock(MovieGateway.class);
-    MovieService  movieService = new MovieService(movieGateway);
+    MovieService movieService = new MovieService(movieGateway);
     movieService.get(1);
     verify(movieGateway).getById(1);
   }
